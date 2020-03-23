f64ed8bffa29047794ccc0f16333119cae180818
Nwosu Onyedikachi <onyedikachinwosu@rocketmail.com>
2020-03-23 16:14:05 +0100

    Secure the app against malicious attacks

--- src/app.js
+++ src/app.js
@@ -1,8 +1,37 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const cors = require('cors');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
 
 const app = express();
+
+// Secure App
+const whitelist = ['http://google.com'];
+const corsOptions = {
+  origin: function(origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+};
+
+app.use(cors(corsOptions));
+
+app.use(helmet());
+app.use(xss());
+
+const limit = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests'
+});
+
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
@@ -16,9 +45,11 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
-app.use(bodyParser.json());
+
+app.use(bodyParser.json({ limit: '10kb' }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('dev'));
+app.use(hpp());
 
 app.get('/', (req, res) => {
   res.status(200).send('Teamwork Backend Server Setup');
@@ -26,7 +57,7 @@ app.get('/', (req, res) => {
 
 const apiRoutes = require('./router');
 
-app.use('/api/v1', apiRoutes);
+app.use('/api/v1', apiRoutes, limit);
 
 app.use((err, req, res, next) => {
   if (err.isBoom) {
